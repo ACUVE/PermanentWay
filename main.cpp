25dce5f87a43bee3b3337b5be59b3ff8d12b502b
ACUVE <sub_chon@yahoo.co.jp>
2014-07-13 17:17:29 +0900

    読み込むファイルが1つの時は std::async を利用しないように変更

--- main.cpp
+++ main.cpp
@@ -18,22 +18,32 @@ bool do_main(int argc, char **argv){
     Option const &opt = optopt.get();
     
     StopWatch sw;
-    s::vector<s::future<b::optional<TrackData>>> futures;
-    for(auto &&f : opt.files){
-        s::cout << f.filename << s::endl;
-        
-        futures.emplace_back(s::async(s::launch::async, [f = f.filename](){
-            StopWatch sw;
-            scope_exit{sw.stop(); sw.show(s::string("parse_file(\"") + f + "\")");};
-            return parse_file(f);
-        }));
-    }
     s::vector<TrackData> data;
-    for(auto &&f : futures){
-        b::optional<TrackData> tr = f.get();
+    if(opt.files.size() > 1){
+        s::vector<s::future<b::optional<TrackData>>> futures;
+        for(auto &&f : opt.files){
+            s::cout << f.filename << s::endl;
+            
+            futures.emplace_back(s::async(s::launch::async, [f = f.filename](){
+                StopWatch sw;
+                scope_exit{sw.stop(); sw.show(s::string("parse_file(\"") + f + "\")");};
+                return parse_file(f);
+            }));
+        }
+        for(auto &&f : futures){
+            b::optional<TrackData> tr = f.get();
+            if(!tr){
+                s::cerr << "Err: parse_file() error" << s::endl;
+                return false;
+            }
+            data.emplace_back(s::move(tr.get()));
+        }
+    }else{
+        StopWatch sw;
+        b::optional<TrackData> tr = parse_file(opt.files.front().filename);
+        scope_exit{sw.stop(); sw.show(s::string("parse_file(\"") + opt.files.front().filename + "\")");};
         if(!tr){
             s::cerr << "Err: parse_file() error" << s::endl;
-            return false;
         }
         data.emplace_back(s::move(tr.get()));
     }
